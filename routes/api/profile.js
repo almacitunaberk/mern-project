758c04182f606b59da318ce4efc2044ff046937a
Tunaberk Almaci <almacitunaberk@gmail.com>
2021-08-02 19:34:30 +0300

    Fetching GitHub repos of a given user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator/check');
+const request = require('request');
+const config = require('config');
 
 const Profile = require('../../models/Profile');
 const USer = require('../../models/User');
@@ -275,5 +277,29 @@ router.delete('/education/:ed_id', auth, async (req,res) => {
         res.status(500).send("Server Error");
     }
 });
+
+// @route   GET api/profile/github/:username
+// @desc    Get User repo from GitHub
+// @acess   Public
+router.get('/github/:username', async (req, res) => {
+    try {
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+            method: 'GET',
+            headers: { 'user-agent': 'node.js' }
+        };
+
+        request(options, (error, response, body) => {
+            if (error) console.error(error);
+            if (response.statusCode !== 200) {
+                return res.status(404).json({msg: "404 Not Found"});
+            }
+            res.json(JSON.parse(body));
+        })
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+})
 
 module.exports = router;
