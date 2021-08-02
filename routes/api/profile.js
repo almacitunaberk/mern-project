fe9515565d5f59fb037d7a668c3abf46e069640d
Tunaberk Almaci <almacitunaberk@gmail.com>
2021-08-02 15:10:50 +0300

    Getting all profiles and getting a profile by user id done

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,6 +5,7 @@ const { check, validationResult } = require('express-validator/check');
 
 const Profile = require('../../models/Profile');
 const USer = require('../../models/User');
+const { route } = require('./users');
 
 // @route   GET api/profile/me
 // @desc    Get current user's profile
@@ -94,6 +95,38 @@ router.post('/', [auth, [
         console.error(err.message);
         res.status(500).send("Server Error");
     }
+});
+
+// @route   GET api/profile
+// @desc    Get all profiles
+// @acess   Public
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }    
+});
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user ID
+// @acess   Public
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar']);
+        if (!profile) {
+            return res.status(400).json({msg: "Profile not found"});            
+        }
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind == 'ObjectId') {
+            return res.status(400).json({msg: "Profile not found"});            
+        }
+        res.status(500).send("Server Error");
+    } 
 });
 
 module.exports = router;
