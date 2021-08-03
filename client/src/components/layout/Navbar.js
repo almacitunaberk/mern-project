dc4ff33b5e99b10c4ace57e62883dd1538d9e264
Tunaberk Almaci <almacitunaberk@gmail.com>
2021-08-03 22:57:32 +0300

    Links and routes done

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
@@ -10,13 +11,13 @@ function Navbar() {
       </h1>
       <ul>
         <li>
-          <a href="profiles.html">Developers</a>
+          <a href="!#">Developers</a>
         </li>
         <li>
-          <a href="register.html">Register</a>
+          <Link to="/register">Register</Link>
         </li>
         <li>
-          <a href="login.html">Login</a>
+          <Link to="/login">Login</Link>
         </li>
       </ul>
     </nav>
