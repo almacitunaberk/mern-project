4cca85eb227642953fb485377413c18a3262fd16
Tunaberk Almaci <almacitunaberk@gmail.com>
2021-08-04 14:41:55 +0300

    Redux store created

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,13 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
 function App() {
   return (
+    <Provider store={store}>
     <Router>
       <Fragment>
         <Navbar />
@@ -20,6 +25,7 @@ function App() {
         </section>
       </Fragment>
     </Router>
+    </Provider>
   );
 }
 
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,31 +1,50 @@
 import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
 
 function Register() {
-  
-    const [formData, setFormData] = useState({
-        name:"",
-        email: "",
-        password: "",
-        password2: ""
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
     });
+  };
 
-    const { name, email, password, password2 } = formData;
-
-    const onChange = (e) => {
-        setFormData({
-            ...formData,
-            [e.target.name]: e.target.value;
-        })
-    };
-
-    const onSubmit = (e) => {
-        e.preventDefault();
-        if (password!== password2) {
-            console.log("Passwords do not match");
-        } else {
-            console.log(formData);
-        }
-    };
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log("Passwords do not match");
+    } else {
+      /*
+      const newUser = {
+        name,
+        email,
+        password,
+      };
+      try {
+        const config = {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+        const body = JSON.stringify(newUser);
+        const res = await axios.post("/api/users", body, config);
+        console.log(res.data);
+      } catch (error) {
+        console.error(error.response.data);
+      }
+      */
+      console.log("SUCCESS");
+    }
+  };
 
   return (
     <Fragment>
@@ -33,12 +52,30 @@ function Register() {
       <p className="lead">
         <i className="fas fa-user"></i> Create Your Account
       </p>
-      <form className="form" onSubmit={e => onSubmit(e)} action="create-profile.html">
+      <form
+        className="form"
+        onSubmit={(e) => onSubmit(e)}
+        action="create-profile.html"
+      >
         <div className="form-group">
-          <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} required />
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={(e) => onChange(e)}
+            required
+          />
         </div>
         <div className="form-group">
-          <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} required/>
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
           <small className="form-text">
             This site uses Gravatar so if you want a profile image, use a
             Gravatar email
@@ -50,7 +87,7 @@ function Register() {
             placeholder="Password"
             name="password"
             value={password}
-            onChange={e => onChange(e)}
+            onChange={(e) => onChange(e)}
             minLength="6"
             required
           />
@@ -61,14 +98,14 @@ function Register() {
             placeholder="Confirm Password"
             name="password2"
             value={password2}
-            onChange={e => onChange(e)}
+            onChange={(e) => onChange(e)}
             minLength="6"
           />
         </div>
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </Fragment>
   );
