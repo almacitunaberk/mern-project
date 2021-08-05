00d21fc4cffd2960e1cfd221bb64ff62cab71d85
Tunaberk Almaci <almacitunaberk@gmail.com>
2021-08-05 20:49:29 +0300

    Create Profile form added

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Login from "./components/auth/Login";
 import PrivateRoute from "./components/routing/PrivateRoute";
 import Alert from "./components/layout/Alert";
 import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/profile-form/CreateProfile";
 import { loadUser } from "./actions/auth";
 import setAuthToken from "./utils/setAuthToken";
 
@@ -37,6 +38,7 @@ function App() {
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
             <PrivateRoute exact path="/dashboard" component={Dashboard} />
+            <PrivateRoute exact path="/create-profile" component={CreateProfile} />
           </Switch>
         </section>
       </Fragment>
