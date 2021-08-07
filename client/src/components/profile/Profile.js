0049e08ccb91af14e52cb32a100316457c3f06ca
Tunaberk Almaci <almacitunaberk@gmail.com>
2021-08-07 19:11:17 +0300

    Profile details added

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,12 +4,14 @@ import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import { getProfileById } from "../../actions/profile";
 import { Link } from "react-router-dom";
+import ProfileTop from "./ProfileTop";
+import ProfileAbout from "./ProfileAbout";
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
     
     useEffect(() => {
         getProfileById(match.params.id);
-    }, [getProfileById]);
+    }, [getProfileById, match.params.id]);
 
     return (
         <Fragment>
@@ -26,6 +28,10 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
                         Edit Profile
                     </Link>
                 )}
+                <div class="profile-grid my-1">
+                    <ProfileTop profile={profile} />
+                    <ProfileAbout profile={profile} />
+                </div>
             </Fragment>
         )}
         </Fragment>
